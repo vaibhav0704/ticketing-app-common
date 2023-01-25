f825efc909738b2693e251d36f55cb7f11b8ed33
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2023-01-26 00:08:11 +0530

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 34,
+  green: 33,
+};
+
+console.log(color);
+
+export default color;
